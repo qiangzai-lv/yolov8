bcaa63a2c3c6322fdd00b6ee77a47820e91a6f69
qiangzai-lv <3486105329@qq.com>
2024-07-07 16:45:29 +0800

    fix

--- val.py
+++ val.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 def main():
     model = YOLO(r'runs/obb/train/weights/best.pt')
-    model.val(data='dota8-obb.yaml', imgsz=1024, batch=4, workers=4)
+    model.val(data='config/dota8-obb.yaml', imgsz=1024, batch=4, workers=4)
 
 
 if __name__ == '__main__':
